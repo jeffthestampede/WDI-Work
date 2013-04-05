f03a534a79a3c5a549ca931484093deefdca7e2a
Jeffrey Kao <jeffthestampede@gmail.com>
2013-04-05 01:36:17 -0700

    styling changes add checkbox

--- sinatra-store/store.rb
+++ sinatra-store/store.rb
@@ -36,9 +36,10 @@ end
 post '/product/new' do
   name = params[:product_name]
   price = params[:product_price]
+  sale = params[:on_sale]
   db = SQLite3::Database.new "store.sqlite3"
   db.results_as_hash = true
-  @rs = db.execute("INSERT INTO products ('name', 'price') VALUES ('#{name}', '#{price}');")
+  @rs = db.execute("INSERT INTO products ('name', 'price', 'on_sale') VALUES ('#{name}', '#{price}', '#{sale}');")
   @name = name
   @price = price
   erb :success
@@ -69,12 +70,14 @@ post '/products/:id/show_form' do
   id = params[:id]
   name = params[:product_name]
   price = params[:product_price]
+  sale = params[:on_sale]
   db = SQLite3::Database.new "store.sqlite3"
   db.results_as_hash = true
-  @rs = db.execute("UPDATE products SET name='#{name}', price=#{price} WHERE id = #{id};")
+  @rs = db.execute("UPDATE products SET name='#{name}', price='#{price}', on_sale='#{sale}' WHERE id = #{id};")
   @id = id
   @name = name
   @price = price
+  @sale = sale
   erb :success
 end
  
