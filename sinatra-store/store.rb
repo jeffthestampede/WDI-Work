6e23d6692e33588b9691a89dee5e01ea1aee3cb8
Jeffrey Kao <jeffthestampede@gmail.com>
2013-04-04 21:49:57 -0700

    styling changes

--- sinatra-store/store.rb
+++ sinatra-store/store.rb
@@ -4,16 +4,6 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
-
-# Add a layout that yields to a template
-# Add a /products route that lists all the products in the store database in an HTML table
-# Add a root route (/) that contains links to the /users and /products pages
-# Add a back button on /users and /products that takes you back to the home page
-# Style the tables using CSS
-# If the product is on sale, display an image in the table
-# Add "zebra stripes" to the table (rows alternate background colors, making them easier to read across) 
-
-
 get '/users' do
   db = SQLite3::Database.new "store.sqlite3"
   db.results_as_hash = true
@@ -54,8 +44,6 @@ post '/product/new' do
   erb :success
 end
 
-
-
 get '/products/:id/delete' do
   id = params[:id]
   @id = id
@@ -68,6 +56,7 @@ post '/products/:id/delete' do
   db = SQLite3::Database.new "store.sqlite3"
   db.results_as_hash = true
   @rs = db.execute("DELETE FROM products WHERE id = #{id};")
+  erb :success
 end
 
 get '/products/:id/show_form' do
@@ -98,23 +87,3 @@ end
  
  
  
- 
- 
- 
-=begin
- 
-  <form method='post' action='/create'>
-    <input type='text' name='name' autofocus>
-    <input type='text' name='photo'>
-    <input type='text' name='breed'>
-    <button>dog me!</button>
-  </form>
- 
- 
-  post '/create' do
-  end
- 
- 
-  redirect '/'
- 
-=end
